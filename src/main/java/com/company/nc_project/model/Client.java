9579f7ec10b544921db6d9038651a42e8626dca4
Nazhadna <skul-nadezhda@ya.ru>
2021-06-25 00:25:07 +0300

    Adding client model

---
+++ src/main/java/com/company/nc_project/model/Client.java
@@ -0,0 +1,31 @@
+package com.company.nc_project.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "client")
+@Data
+public class Client {
+
+    @Id
+    @org.hibernate.annotations.Type(type = "pg-uuid")
+    private UUID id = UUID.randomUUID();
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    @Column(name = "gender", nullable = false)
+    private Character gender;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
