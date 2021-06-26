6eb9c47aca5a528493528c7d963f06c1e5e4c10e
Nazhadna <skul-nadezhda@ya.ru>
2021-06-26 14:07:16 +0300

    Changed ExceptionHandler

--- src/main/java/com/company/nc_project/controllers/ClientController.java
+++ src/main/java/com/company/nc_project/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.company.nc_project.controllers;
 import com.company.nc_project.model.Client;
 import com.company.nc_project.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
@@ -41,16 +42,11 @@ public class ClientController {
 
         clientRepository.deleteById(clientId);
 
-        Client client = clientRepository.findById(clientId)
-                .orElseThrow(() -> new EntityNotFoundException("Contact not found for id : " + clientId));
-
-        clientRepository.delete(client);
-
         return "Client with id " + clientId + " deleted";
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public String handleException(EntityNotFoundException e) {
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public String handleException(EmptyResultDataAccessException e) {
         return "error: " + e.getMessage();
     }
 }
