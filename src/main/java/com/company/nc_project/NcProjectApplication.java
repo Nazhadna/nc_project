fd050cac9e380fc47f10a0e60f6c832d4d1a9986
Nazhadna <skul-nadezhda@ya.ru>
2021-07-04 16:15:02 +0300

    Update

--- src/main/java/com/company/nc_project/NcProjectApplication.java
+++ src/main/java/com/company/nc_project/NcProjectApplication.java
@@ -2,7 +2,6 @@ package com.company.nc_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
