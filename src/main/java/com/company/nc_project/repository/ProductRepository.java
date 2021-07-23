c65e9631bc101842b4d9ddc0a5bd1dbf5667dafe
Nazhadna <skul-nadezhda@ya.ru>
2021-07-23 15:15:45 +0300

    Updated query with quantity of products

--- src/main/java/com/company/nc_project/repository/ProductRepository.java
+++ src/main/java/com/company/nc_project/repository/ProductRepository.java
@@ -10,10 +10,11 @@ import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, UUID> {
-    @Query(value = "SELECT id, name, lifetime, place_id FROM product JOIN " +
-            "(SELECT dish.product_id FROM (SELECT product_id FROM dish_product WHERE dish_id=?2) AS dish " +
-            "LEFT JOIN (SELECT product_id FROM stored_product  WHERE client_id=?1) AS available_products " +
-            "ON dish.product_id=available_products.product_id WHERE available_products.product_id IS NULL) AS needed_products " +
+    @Query(value = "SELECT id, name, lifetime, place_id, units_id FROM product JOIN " +
+            "(SELECT dish.product_id FROM (SELECT product_id, quantity FROM dish_product WHERE dish_id=?2) AS dish " +
+            "LEFT JOIN (SELECT product_id, SUM(quantity) AS quantity FROM stored_product  WHERE client_id=?1 GROUP BY product_id) " +
+            "AS available_products ON dish.product_id=available_products.product_id WHERE available_products.product_id IS NULL " +
+            "OR dish.quantity>available_products.quantity) AS needed_products " +
             "ON product.id=needed_products.product_id;", nativeQuery = true)
     Set<Product> getNeededProducts(UUID client_id, UUID dish_id);
 }
