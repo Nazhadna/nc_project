694eaf4e4d55b150da6334e3d4b387504515a5c8
Nazhadna <skul-nadezhda@ya.ru>
2021-07-31 13:17:20 +0300

    Add authentication

---
+++ src/main/java/com/company/nc_project/model/Role.java
@@ -0,0 +1,31 @@
+package com.company.nc_project.model;
+
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum Role {
+
+    USER(new HashSet<>(Arrays.asList(Permission.READ))),
+    ADMIN(new HashSet<>(Arrays.asList(Permission.READ, Permission.WRITE)));
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthority() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
+}
